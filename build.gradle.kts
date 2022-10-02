596b5567fe675dceb3d28791b4f3baef05af7506
sya-ri <git@s7a.dev>
2022-10-02 12:58:37 +0900

    chore: Setup spigot-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,11 @@ version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/groups/public/")
 }
 
 dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
     testImplementation(kotlin("test"))
 }
 
