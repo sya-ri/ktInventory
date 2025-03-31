16bfa3c3f7966ae07043f4742795cb28661c8677
sya-ri <git@s7a.dev>
2025-03-31 19:54:03 +0900

    refactor: Remove unnecessary receiver

--- src/main/kotlin/dev/s7a/ktinventory/util/GetAllViewers.kt
+++ src/main/kotlin/dev/s7a/ktinventory/util/GetAllViewers.kt
@@ -4,7 +4,7 @@ import dev.s7a.ktinventory.KtInventoryBase
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
-inline fun <reified T : KtInventoryBase> T.getAllViewers(): List<Player> =
+inline fun <reified T : KtInventoryBase> getAllViewers(): List<Player> =
     Bukkit.getOnlinePlayers().filter {
         it.openInventory.topInventory is T
     }
