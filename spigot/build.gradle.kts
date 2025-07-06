5d23d15a7de1109429cc63dad155f6eaed323d1e
sya-ri <git@s7a.dev>
2025-07-07 02:35:45 +0900

    chore: Remove unused files

--- spigot/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-    api(project(":"))
-
-    if (project.hasProperty("USE_SPIGOT_8")) {
-        compileOnly(libs.spigot8)
-    } else {
-        compileOnly(libs.spigotLatest)
-    }
-}
