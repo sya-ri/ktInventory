4cf1e3d48c6af859ef0d7f35766d8bc41949dffe
sya-ri <git@s7a.dev>
2025-07-07 02:42:39 +0900

    chore: Fix publishes settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,9 +53,9 @@ publishing {
             url =
                 uri(
                     if (version.toString().endsWith("SNAPSHOT")) {
-                        "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                        "https://central.sonatype.com/repository/maven-snapshots/"
                     } else {
-                        "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                        "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
                     },
                 )
             credentials {
