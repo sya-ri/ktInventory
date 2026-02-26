deab5bf76bb64731b63e2883d110eb7cee6ff92f
sya-ri <git@s7a.dev>
2026-02-26 15:12:17 +0900

    refactor: Move `KtInventoryPluginContext` factory function to companion object for better encapsulation

--- src/main/kotlin/dev/s7a/ktinventory/KtInventoryPluginContext.kt
+++ src/main/kotlin/dev/s7a/ktinventory/KtInventoryPluginContext.kt
@@ -4,19 +4,6 @@ import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
 
 /**
- * Creates a [KtInventoryPluginContext] instance for the specified [plugin].
- *
- * @since 2.1.0
- */
-@Suppress("FunctionName")
-fun KtInventoryPluginContext(plugin: Plugin) =
-    object : KtInventoryPluginContext {
-        override fun registerEvents(listener: Listener) {
-            plugin.server.pluginManager.registerEvents(listener, plugin)
-        }
-    }
-
-/**
  * Context interface for KtInventory plugin integration.
  *
  * This interface provides methods for registering event listeners within the plugin context.
@@ -31,4 +18,19 @@ interface KtInventoryPluginContext {
      * @since 2.1.0
      */
     fun registerEvents(listener: Listener)
+
+    companion object {
+        /**
+         * Creates a [KtInventoryPluginContext] instance for the specified [plugin].
+         *
+         * @since 2.1.0
+         */
+
+        operator fun invoke(plugin: Plugin) =
+            object : KtInventoryPluginContext {
+                override fun registerEvents(listener: Listener) {
+                    plugin.server.pluginManager.registerEvents(listener, plugin)
+                }
+            }
+    }
 }
